e2bba786c77e6a7353bb18f1539b68d303486051
Simone Sava <io@sps.me>
2022-08-25 11:18:16 +0200

    Removed unused method

--- N4Lib/WeightsInitializer.cs
+++ N4Lib/WeightsInitializer.cs
@@ -7,9 +7,6 @@ public sealed class WeightsInitializer
         => _rng = new Random();
 
     // randn(layer_size, layer_size-1)
-    public double Next()
-        => _rng.NextDouble();
-
     public double Next(IActivationFunction layerActivationFunction, int layerSize)
-        => Next() * layerActivationFunction.OptimizationFactor(layerSize);
+        => _rng.NextDouble() * layerActivationFunction.OptimizationFactor(layerSize);
 }
