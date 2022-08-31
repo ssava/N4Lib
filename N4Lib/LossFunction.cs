12cc3f2563addf94e7369dd176c03a0e1cecfbd4
Simone Sava <io@sps.me>
2022-08-31 13:50:16 +0200

    Added supported loss functions

--- N4Lib/LossFunction.cs
+++ N4Lib/LossFunction.cs
@@ -2,8 +2,41 @@ namespace N4Lib;
 
 public sealed class LossFunction
 {
-    public static readonly LossFunction MinSquares
+    /// <summary>
+    /// Mean Squared Error (MSE for short)
+    /// This loss function is generally used for regression tasks.
+    /// </summary>
+    /// <returns>MSE loss function instance</returns>
+    public static readonly LossFunction MeanSquaredError
         = new(x => x * 2, x => 2);
+
+    /// <summary>
+    /// Binary Cross-Entropy (BCE for short).
+    /// This loss function is used for binary classification.
+    /// </summary>
+    /// <returns>BCE loss function instance</returns>
+    /// <remarks>This function need ONE output node and Sigmoid function</remarks>
+    public static readonly LossFunction BinaryCrossEntropy
+        => new(x => x, x => x);
+
+    /// <summary>
+    /// Categorical Cross-Entropy (CCE for short).
+    /// This loss function is used for multi/class classification.
+    /// </summary>
+    /// <returns>CCE loss function instance</returns>
+    /// <remarks>This function need N output, where N is the number of classes and output node should use SoftMax function</remarks>
+    public static readonly LossFunction CategoricalCrossEntropy
+        => new(x => x, x => x);
+
+    /// <summary>
+    /// Sparse Categorical Cross-Entropy (SCCE for short).
+    /// This loss function is used for multi/class classification.
+    /// Is similar to CCE but One-Hot Encode for output is not necessary
+    /// </summary>
+    /// <returns>CCE loss function instance</returns>
+    /// <remarks>This function need N output, where N is the number of classes and output node should use SoftMax function</remarks>
+    public static readonly LossFunction SparseCategoricalCrossEntropy
+        => new(x => x, x => x);
 
     private readonly Func<double, double> _function;
 
